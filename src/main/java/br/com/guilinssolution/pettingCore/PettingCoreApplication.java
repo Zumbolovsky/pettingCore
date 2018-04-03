3e386d9abf283ab3cb256a9a5acf0c9e211f6d58
Andrew <a.siquieri@hotmail.com>
2018-04-03 00:35:01 -0300

    Commit Inicial

---
+++ src/main/java/br/com/guilinssolution/pettingCore/PettingCoreApplication.java
@@ -0,0 +1,12 @@
+package br.com.guilinssolution.pettingCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettingCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettingCoreApplication.class, args);
+	}
+}
